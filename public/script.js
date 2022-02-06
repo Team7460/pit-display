7709dcd459b4ca616b646b81927a9db7b3ef0b8a
AviAndFriends <69689741+AviAndFriends@users.noreply.github.com>
2022-02-06 18:51:20 +0000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,10 @@
-const matches = [];
+let matches = [];
 
 function getDates(value) {
-
   return value.predicted_time * 1000
 }
 
 function getNums(value) {
-
   return value.match_number
 }
 
@@ -16,7 +14,7 @@ function sortNumber(a, b) {
 
 function loadJSON(callback) {
 
-  var xobj = new XMLHttpRequest();
+  const xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open('GET', 'tbaData.json', true); // Replace 'my_data' with the path to your file
   xobj.onreadystatechange = function () {
@@ -27,116 +25,102 @@ function loadJSON(callback) {
   };
   xobj.send(null);
 }
-var tbaData;
+
+let tbaData;
 loadJSON(function (response) {
       // Parse JSON string into object
       tbaData = JSON.parse(response);
-      var matches = tbaData.map(getDates);
-      var matchNos = tbaData.map(getNums)
-      matches.sort(sortNumber);
+  const matches = tbaData.map(getDates);
+  const matchNos = tbaData.map(getNums);
+  matches.sort(sortNumber);
       matchNos.sort(sortNumber);
 
+  let leadingZero;
+  let mLeadingZero;
+  let hLeadingZero;
 
-      var leadingZero;
-      var mLeadingZero;
-      var hLeadingZero;
+  let countDownDate = new Date('Dec 31, 2099 23:59:59').getTime();
+  let nextMatch;
 
-      var countDownDate = new Date("Dec 31, 2099 23:59:59").getTime();
-      var nextMatch;
+  const shiftLoop = setInterval(function () {
+    const now = new Date().getTime();
+    matches.sort(sortNumber);
+    matchNos.sort(sortNumber)
 
-      var shiftLoop = setInterval(function () {
-        var now = new Date().getTime();
-        matches.sort(sortNumber);
-        matchNos.sort(sortNumber)
+    countDownDate = matches[0];
+    nextMatch = matchNos[0];
 
+    if (countDownDate < now) {
+      matches.shift();
+      matchNos.shift();
+      countDownDate = matches[0];
+      nextMatch = matches[0];
+    } else {
+      clearInterval(shiftLoop);
+    }
+  }, 100);
 
-        countDownDate = matches[0];
-        nextMatch = matchNos[0];
+  // Update the count down every 1 second
+  const x = setInterval(function () {
+    matches.sort(sortNumber);
+    matchNos.sort(sortNumber)
 
-        if (countDownDate < now) {
-          matches.shift();
-          matchNos.shift();
-          countDownDate = matches[0];
-          nextMatch = matches[0];
-        } else {
-          clearInterval(shiftLoop);
-        }
-      }, 100);
+    countDownDate = matches[0];
+    nextMatch = matchNos[0];
 
+    // Get todays date and time
+    const now = new Date().getTime();
 
+    if (countDownDate < now) {
+      matches.shift();
+      matchNos.shift();
+      countDownDate = matches[0];
+      nextMatch = matches[0];
+    }
 
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
 
-      // Update the count down every 1 second
-      var x = setInterval(function () {
-        matches.sort(sortNumber);
-        matchNos.sort(sortNumber)
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + days * 24;
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    if (seconds < 10) {
+      leadingZero = '0'
+    } else {
+      leadingZero = '';
+    }
 
-        countDownDate = matches[0];
-        nextMatch = matchNos[0];
+    if (minutes < 10) {
+      mLeadingZero = '0';
+    } else {
+      mLeadingZero = '';
+    }
 
+    if (hours < 10) {
+      hLeadingZero = '0';
+    } else {
+      hLeadingZero = '';
+    }
 
+    if (seconds <= -1) {
+      matches.shift();
+      matchNos.shift();
+    }
 
+    if (minutes < 15 && hours == 0 && days == 0) {
 
+      document.getElementById('clock').innerHTML = 'Queue';
+    } else if (countDownDate == undefined) {
+      document.getElementById('clock').innerHTML = `Fin`;
 
-        // Get todays date and time
-        var now = new Date().getTime();
+    } else {
+      document.getElementById('clock').innerHTML = hLeadingZero + hours + ':' + mLeadingZero +
+        minutes + ':' + leadingZero + seconds;
 
-        if (countDownDate < now) {
-          matches.shift();
-          matchNos.shift();
-          countDownDate = matches[0];
-          nextMatch = matches[0];
-        }
+    }
 
-
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + days * 24;
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-
-        if (seconds < 10) {
-          leadingZero = "0"
-        } else {
-          leadingZero = "";
-        }
-
-        if (minutes < 10) {
-          mLeadingZero = "0";
-        } else {
-          mLeadingZero = "";
-        }
-
-        if (hours < 10) {
-          hLeadingZero = "0";
-        } else {
-          hLeadingZero = "";
-        }
-
-
-        if (seconds <= -1) {
-          matches.shift();
-          matchNos.shift();
-        }
-
-        if (minutes < 15 && hours == 0 && days == 0) {
-
-          document.getElementById("clock").innerHTML = "Queue";
-        } else if (countDownDate == undefined) {
-          document.getElementById("clock").innerHTML = `Fin`;
-
-        } else {
-          document.getElementById("clock").innerHTML = hLeadingZero + hours + ":" + mLeadingZero +
-            minutes + ":" + leadingZero + seconds;
-
-        }
-
-      
-      }, 1000);
-    });
+  }, 1000);
+});
